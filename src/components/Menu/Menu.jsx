566300a8cbc0bfb2e9ffd6d3861b5b99feb72a7a
Petra Venäläinen-Kuusela <petri.venalainen@protonmail.com>
2025-01-08 10:16:02 +0200

    iconit lisätty työhön

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,20 @@
 import styles from './Menu.module.scss'
-import { MdViewList } from 'react-icons/md'
+import { MdViewList, MdSettings, MdTimeline } from 'react-icons/md';
 
 function Menu() {
-
-  return (
-    <div className={styles.menu}>
-      <div><MdViewList /></div>
-      <div>stats</div>
-      <div>profile</div>
-    </div>
-  )
-
+    return (
+        <div className={styles.menu}>
+            <div>
+                <MdViewList size={24} />
+            </div>
+            <div>
+                <MdTimeline size={24} />
+            </div>
+            <div>
+                <MdSettings size={24} />
+            </div>
+        </div>
+    );
 }
 
-export default Menu
+export default Menu;

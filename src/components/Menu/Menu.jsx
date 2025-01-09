75b4cc2cc78d7e7a7445c35c794915cac64179a0
Petra Venäläinen-Kuusela <petri.venalainen@protonmail.com>
2025-01-09 14:22:54 +0200

    lisää linkityksen Menu-komponenttiin

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,19 +1,16 @@
+import { NavLink } from 'react-router-dom'
 import styles from './Menu.module.scss'
 import { MdViewList, MdSettings, MdTimeline } from 'react-icons/md';
 
 function Menu() {
     return (
         <div className={styles.menu}>
-            <div>
-                <MdViewList size={24} />
-            </div>
-            <div>
-                <MdTimeline size={24} />
-            </div>
-            <div>
-                <MdSettings size={24} />
-            </div>
-        </div>
+        <div><NavLink to=""><MdViewList /></NavLink></div>
+        <div><NavLink to="/stats"><MdTimeline /></NavLink></div>
+        <div><NavLink to="/settings"><MdSettings /></NavLink></div>
+      </div>
+  
+
     );
 }
 

d6240e22680428f2863365b74bda8dcf13af999f
Petra Venäläinen-Kuusela <petri.venalainen@protonmail.com>
2025-01-09 14:20:05 +0200

    lisää AppRouter-komponentin ja reitityksen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,12 @@
-import Settings from '../Settings'
-import { ButtonContainer } from '../../shared/buttons'
-import Content from '../Content'
-import Header from '../Header'
-import styles from './App.module.scss'
-import Items from '../Items'
-import Menu from '../Menu'
-import { FloatingButton } from '../../shared/buttons'
-import Stats from '../Stats'
-
-
-
+import AppRouter from '../AppRouter'
 
 function App() {
 
   return (
     <>
-      <ButtonContainer>
-        <div className={styles.app}>
-          <Header />
-          <Content>
-          <Settings />
-          </Content>
-          <Menu />
-        </div>
-      </ButtonContainer>
+      <AppRouter />
     </>
   )
-
-
-
 }
 
 export default App
